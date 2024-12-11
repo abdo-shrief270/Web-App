9f08199e03fe71970ef72279c648a9cb517c2902
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2024-12-11 20:43:11 +0200

    update app

--- main.js
+++ main.js
@@ -70,3 +70,19 @@ else{
         circle.style.transition='1s';
     }
 }
+let sidebarbtn = document.getElementById("sidebar-btn");
+let sidebar = document.querySelector(".sidebar");
+let xbar = document.getElementById("xbar");
+let xmark = document.getElementById("xmark");
+ 
+
+xmark.classList.remove("fa-circle-xmark");
+
+
+sidebarbtn.onclick = function (){
+  sidebar.classList.toggle("side-move");
+  sidebarbtn.classList.toggle("btn-move");
+  xmark.classList.toggle("fa-circle-xmark");
+  xbar.classList.toggle("fa-bars");
+  sidebarbtn.style.border="0px";
+};
--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-let control=document.getElementById('control');
-let moving=document.getElementById('moving');
-let dis=document.getElementById('dis');
-let appear=document.getElementById('appear');
-moving.classList.add('hide');
-control.onclick=function()
-{   
-    moving.classList.toggle('hide');
-    dis.classList.toggle('hide');
-    appear.classList.toggle('hide');
-    control.classList.toggle('anime');  
-}
-
-
-
 let sidebarbtn = document.getElementById("sidebar-btn");
 let sidebar = document.querySelector(".sidebar");
 let xbar = document.getElementById("xbar");
