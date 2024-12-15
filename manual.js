c1a4eb880e855b9b055bd55f9ccc93a50bafc069
Mhoss33 <mh8917416@gmail.com>
2024-12-15 19:30:46 +0200

    SIDE-BAR update for all the pages

--- manual.js
+++ manual.js
@@ -17,15 +17,15 @@ let sidebarbtn = document.getElementById("sidebar-btn");
 let sidebar = document.querySelector(".sidebar");
 let xbar = document.getElementById("xbar");
 let xmark = document.getElementById("xmark");
- 
+sidebar.classList.add("hide");
 
 xmark.classList.remove("fa-circle-xmark");
-
 
 sidebarbtn.onclick = function (){
   sidebar.classList.toggle("side-move");
   sidebarbtn.classList.toggle("btn-move");
   xmark.classList.toggle("fa-circle-xmark");
   xbar.classList.toggle("fa-bars");
+  sidebar.classList.toggle("hide");
   sidebarbtn.style.border="0px";
 };
